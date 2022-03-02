ffe17b0369ccc5adcf77484e404b90ece868823b
CedRic <56505305+Canaill51@users.noreply.github.com>
2022-03-02 10:29:35 +0100

    Update CFPV Branch

--- indexer/Settings.js
+++ indexer/Settings.js
@@ -10,11 +10,9 @@ const MetadataTypes = {
         FILE_PATH_ARRAY:    "FILE_PATH_ARRAY", // array of path/to/file.ext and check if files exist
         PRESET_STATUS:      "PRESET_STATUS", // official/community/experimental
         PRIORITY:           "PRIORITY", // 0..99
-        PARSER:             "PARSER", // TEXT, MARKED
 }
 
 const PresetStatusEnum = ["OFFICIAL", "COMMUNITY", "EXPERIMENTAL"];
-const ParserEnum =       ["TEXT", "MARKED"];
 
 const PresetCategories = {
     TUNE:           "TUNE",
@@ -65,7 +63,6 @@ const settings = {
     OptionsDirectives : Object.freeze(OptionsDirectives),
 
     PresetStatusEnum : Object.freeze(PresetStatusEnum),
-    ParserEnum : Object.freeze(ParserEnum),
 
     presetsDir: "presets",
     presetsFileEncoding: "utf-8",
@@ -87,7 +84,6 @@ const settings = {
         include_disclaimer:   {type: MetadataTypes.FILE_PATH_ARRAY,  optional: true   },
         priority:             {type: MetadataTypes.PRIORITY,         optional: true   },
         force_options_review: {type: MetadataTypes.BOOLEAN,          optional: true   },
-        parser:               {type: MetadataTypes.PARSER,           optional: true   },
     }),
 }
 
